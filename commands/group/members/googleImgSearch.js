774eaf17ac1fd4adff2bd62bc8dbfec9e1c77422
Mahesh Kumar <jacktheboss2.2.0@gmail.com>
2022-11-07 15:45:10 +0530

    fixed

--- commands/group/members/googleImgSearch.js
+++ commands/group/members/googleImgSearch.js
@@ -1,5 +1,5 @@
 const gis = require("g-i-s");
-
+const { getGroupData } = require('../../../mongo-DB/groupDataDb');
 module.exports.command = () => {
     let cmd = ["img"];
     return { cmd, handler };
@@ -7,6 +7,8 @@ module.exports.command = () => {
 
 const handler = async (sock, msg, from, args, msgInfoObj) => {
     const { sendMessageWTyping } = msgInfoObj;
+    let data = await getGroupData(from);
+    if (data.isImgOn == false) return sendMessageWTyping(from, { text: "By Default Search Image is Disable in this group." }, { quoted: msg })
     if (!args[0] || msg.message.extendedTextMessage) return sendMessageWTyping(from, { text: "Enter Word to Search" }, { quoted: msg });
     gis(args[0], (err, res) => {
         if (err) return sendMessageWTyping(from, { text: err.toString() }, { quoted: msg });

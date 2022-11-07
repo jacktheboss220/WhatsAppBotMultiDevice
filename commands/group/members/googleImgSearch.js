0f5f52c54172fd2602da9528c14059b33e6416d2
Mahesh Kumar <jacktheboss2.2.0@gmail.com>
2022-11-07 15:59:42 +0530

    &&

--- commands/group/members/googleImgSearch.js
+++ commands/group/members/googleImgSearch.js
@@ -6,11 +6,11 @@ module.exports.command = () => {
 }
 
 const handler = async (sock, msg, from, args, msgInfoObj) => {
-    const { sendMessageWTyping } = msgInfoObj;
+    const { sendMessageWTyping, evv } = msgInfoObj;
     let data = await getGroupData(from);
     if (data.isImgOn == false) return sendMessageWTyping(from, { text: "By Default Search Image is Disable in this group." }, { quoted: msg })
-    if (!args[0] || msg.message.extendedTextMessage) return sendMessageWTyping(from, { text: "Enter Word to Search" }, { quoted: msg });
-    gis(args[0], (err, res) => {
+    if (!args[0] && msg.message.extendedTextMessage) return sendMessageWTyping(from, { text: "Enter Word to Search" }, { quoted: msg });
+    gis(evv, (err, res) => {
         if (err) return sendMessageWTyping(from, { text: err.toString() }, { quoted: msg });
         let count = Math.ceil(Math.random() * res.length);
         sendMessageWTyping(from, { image: { url: res[count].url } }, { quoted: msg })

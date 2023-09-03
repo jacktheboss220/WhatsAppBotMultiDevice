b851f05bdc72636c1f8f7fdeac89d986adef72db
jacktheboss220 <jacktheboss2.2.0@gmail.com>
2023-09-03 19:13:35 +0530

    updated

--- commands/group/members/imgGen.js
+++ commands/group/members/imgGen.js
@@ -1,14 +1,15 @@
-const { Configuration, OpenAIApi } = require("openai");
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+const OpenAI = require('openai');
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
 });
-const openai = new OpenAIApi(configuration);
+
 
 const handler = async (sock, msg, from, args, msgInfoObj) => {
     const { sendMessageWTyping, evv } = msgInfoObj;
     //return sendMessageWTyping(from, { text: "This command is currently disabled." }, { quoted: msg });
     if (!args[0]) return sendMessageWTyping(from, { text: "Please provide a prompt to generate an image from." }, { quoted: msg });
-    await openai.createImage({
+    await openai.images.generate({
         prompt: evv,
         n: 1,
         size: "1024x1024",

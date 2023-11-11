479c3c8240ff6df20fa9d5caf24d65bf70461237
Dashrath <110444186+Dashu0@users.noreply.github.com>
2023-11-11 15:47:42 +0530

    Update stickerForward.js

--- stickerForward.js
+++ stickerForward.js
@@ -6,7 +6,7 @@ const getRandom = (ext) => { return `${Math.floor(Math.random() * 10000)}${ext}`
 
 const forwardGroup = "120363040592884772@g.us"; // Group ID
 
-const ignoreGroup = ["120363180984721796@g.us","120363026740649099@g.us"]; // Group ID with commas
+const ignoreGroup = ["120363180984721796@g.us","120363026740649099@g.us"," 120363180984721796@g.us"]; // Group ID with commas
 
 const stickerLengthArray = [];
 

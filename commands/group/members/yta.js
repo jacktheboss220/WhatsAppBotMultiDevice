edc19cf1be417e481ad051b6265d9d5d2686b3e8
Mahesh Kumar <jacktheboss2.2.0@gmail.com>
2023-12-07 19:03:57 +0530

    Fix formatting issues and update dependencies

--- commands/group/members/yta.js
+++ commands/group/members/yta.js
@@ -8,7 +8,6 @@ module.exports.command = () => {
     return { cmd, handler };
 }
 
-
 const handler = async (sock, msg, from, args, msgInfoObj) => {
     const { evv, command, sendMessageWTyping } = msgInfoObj;
 
@@ -16,7 +15,7 @@ const handler = async (sock, msg, from, args, msgInfoObj) => {
 
     (async () => {
         try {
-            let sany = getRandom('.mp3')
+            let sany = getRandom('.mp3');
             const stream = ytdl(args[0], { filter: info => info.audioBitrate == 160 || info.audioBitrate == 128 })
                 .pipe(fs.createWriteStream(sany));
             console.log("Audio downloaded")
@@ -24,13 +23,9 @@ const handler = async (sock, msg, from, args, msgInfoObj) => {
                 stream.on('error', reject)
                 stream.on('finish', resolve)
             }).then(async (res) => {
-                await sock.sendMessage(
-                    from,
-                    {
-                        audio: fs.readFileSync(sany),
-                    },
-                    { quoted: msg }
-                ).then(() => {
+                await sock.sendMessage(from, {
+                    audio: fs.readFileSync(sany),
+                }, { quoted: msg }).then(() => {
                     console.log("Sent");
                     try {
                         fs.unlinkSync(sany)

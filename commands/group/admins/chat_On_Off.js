e184bb75fa9af21e7aa4150a17e1d6b253e2a9b4
Dashrath <110444186+Dashu0@users.noreply.github.com>
2024-02-26 11:36:09 +0530

    Update chat_On_Off.js

--- commands/group/admins/chat_On_Off.js
+++ commands/group/admins/chat_On_Off.js
@@ -10,10 +10,10 @@ const handler = async (sock, msg, from, args, msgInfoObj) => {
 
     args[0] = args[0].toLowerCase();
     try {
-        if (args[0] === 'off,mute') {
+        if (args[0] === 'off') {
             sock.groupSettingUpdate(from, 'announcement');
             sendMessageWTyping(from, { text: `✔️ *Only Admin can send Message*` }, { quoted: msg });
-        } else if (args[0] === 'on,unmute') {
+        } else if (args[0] === 'on') {
             sock.groupSettingUpdate(from, 'not_announcement');
             sendMessageWTyping(from, { text: `✔️ *Allowed all member can send Message*` }, { quoted: msg });
         } else {
@@ -25,4 +25,4 @@ const handler = async (sock, msg, from, args, msgInfoObj) => {
     }
 };
 
-module.exports.command = () => ({ cmd: ["chat","mute","unmute"], handler })
+module.exports.command = () => ({ cmd: ["chat"], handler })

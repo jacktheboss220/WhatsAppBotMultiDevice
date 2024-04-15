828fc9384f0ebd885bafc029a74f20a972bc1159
Mahesh Kumar <jacktheboss2.2.0@gmail.com>
2024-04-15 16:59:28 +0530

    Update response time formatting in alive.js

--- commands/public/alive.js
+++ commands/public/alive.js
@@ -5,7 +5,7 @@ const handler = async (sock, msg, from, args, msgInfoObj) => {
     // Response time calculation
     const diff = process.hrtime(start);
     const responseTime = (diff[0] * 1e9 + diff[1]) / 1e6;
-    const response = `*🎾 Pong!*\n\n*Response Time:* ${responseTime.toFixed(2)}ms\n*Uptime:* ${uptime.toFixed(2)}s`;
+    const response = `*🎾 Pong!*\n\n*Response Time:* ${responseTime}ms\n*Uptime:* ${uptime.toFixed(2)}s`;
     return sendMessageWTyping(from, { text: response }, { quoted: msg });
 };
 

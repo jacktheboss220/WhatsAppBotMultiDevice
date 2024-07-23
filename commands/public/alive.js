a89064e0d881e7f25d53c5e0cf44dcb8af49895b
Vivek Kumar <scorpianva@gmail.com>
2024-07-23 11:09:35 +0530

    Update dependencies and fix file download bug

--- commands/public/alive.js
+++ commands/public/alive.js
@@ -2,10 +2,14 @@ const handler = async (sock, msg, from, args, msgInfoObj) => {
     const start = process.hrtime();
     const { sendMessageWTyping } = msgInfoObj;
     const uptime = process.uptime();
+    const hours = Math.floor(uptime / 3600);
+    const minutes = Math.floor((uptime % 3600) / 60);
+    const seconds = Math.floor(uptime % 60);
+    const simpleUptime = `${hours}h ${minutes}m ${seconds}s`;
     // Response time calculation
     const diff = process.hrtime(start);
     const responseTime = (diff[0] * 1e9 + diff[1]) / 1e6;
-    const response = `*🎾 Pong!*\n\n*Response Time:* ${responseTime}ms\n*Uptime:* ${uptime.toFixed(2)}s`;
+    const response = `*🎾 Pong!*\n\n*Response Time:* ${responseTime}ms\n*Uptime:* ${simpleUptime}`;
     return sendMessageWTyping(from, { text: response }, { quoted: msg });
 };
 
